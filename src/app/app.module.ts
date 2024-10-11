fc1dfb16a967545a670b88f815c425bb7789cf05
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-10-11 20:37:15 +0300

    Converted HomeComponent to module-based, disabled standalone component generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent  // Declare AppComponent here
+    AppComponent,
+    HomeComponent  // Declare AppComponent here
   ],
   imports: [
     BrowserModule  // Import necessary Angular modules
