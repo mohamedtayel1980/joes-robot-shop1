e98ceeba549dd86708664f99418ad711f5946402
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-10-12 22:17:51 +0300

    add catalog component and site-heade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent  // Declare AppComponent here
+    HomeComponent,
+    CatalogComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule  // Import necessary Angular modules
