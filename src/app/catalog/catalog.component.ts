0bb52c19d28d9b2f511a02740ef5435046854db3
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-10-20 00:07:51 +0300

    The NgClass Directive add ngClass getDiscountedClasses

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -194,4 +194,9 @@ export class CatalogComponent {
     ?this.products
     :this.products.filter((product)=>product.category===this.filter);
   }
+  getDiscountedClasses(product:IProduct){
+    if(product.discount>0) return ['strikethrough']
+    return [];
+
+  }
 }
