bbd2fea9ad9798f86326d7c309d766c1d39fd8dd
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-10-30 23:20:02 +0300

    add post to card

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   cart: IProduct[]=[];
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   add(product: IProduct) {
-    const isAlreadyInCart = this.cart.some(item => item.id === product.id);
+    /*const isAlreadyInCart = this.cart.some(item => item.id === product.id);
     if (!isAlreadyInCart) {
       this.cart.push(product);
       console.log(`Product "${product.name}" added to cart.`);
     } else {
       console.log(`Product "${product.name}" is already in the cart.`);
-    }
+    }*/
+   this.cart.push(product);
+   this.http.post('/api/cart',this.cart).subscribe(()=>{
+    console.log(`Product "${product.name}" added to cart.`);
+   })
   }
 }
