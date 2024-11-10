6f26b0531a846c5454ab45ced1123a3087ded0ed
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-11-10 13:31:30 +0200

    add sign-in commpoenet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 
 const routs:Routes=[
   {path: 'home',component:HomeComponent,title:"Home- joes-robot-shop"},
   {path: 'catalog',component:CatalogComponent,title:"Catalog- joes-robot-shop"},
   {path: 'cart',component:CartComponent,title:"Cart- joes-robot-shop"},
+  {path: 'sign-in',component:SignInComponent},
   {path: '', redirectTo:'/home', pathMatch: 'full'}
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
@@ -8,6 +10,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { provideHttpClient } from '@angular/common/http';
 import { CartComponent } from './cart/cart.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 //import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
@@ -16,11 +19,12 @@ import { AppRoutingModule } from './app-routing.module';
     CatalogComponent,
     SiteHeaderComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule 
+    AppRoutingModule,FormsModule 
     
   ],
   
