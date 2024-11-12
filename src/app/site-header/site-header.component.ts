d9131735751ac349ddf6d4120b07399d203047b9
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-11-12 23:06:08 +0200

    finish sgin-in re-direct

--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from '../user/user.model';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-site-header',
   templateUrl: './site-header.component.html',
   styleUrls: ['./site-header.component.css']
 })
-export class SiteHeaderComponent {
+export class SiteHeaderComponent implements OnInit{
+user:IUser | null=null;
+showSignOutMenu:boolean=false;
+  constructor(private userService:UserService) { }
+  ngOnInit(): void {
+    this.userService.getUser().subscribe({
+      next:(user)=> {this.user=user}
+    })
+  }
 
-  constructor() { }
+  toggleSignOutMenu(){
+    this.showSignOutMenu=!this.showSignOutMenu;
+  }
 
+  signOut(){
+    this.userService.signOut();
+    this.showSignOutMenu=false;
+  }
 }
