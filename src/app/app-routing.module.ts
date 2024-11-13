dac2ab9d81e54b7a8ee4146560b9cc68a8b8a9fe
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-11-13 18:04:07 +0200

    add multi validators

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
 import { SignInComponent } from './user/sign-in/sign-in.component';
+import { TemplateFormControlsComponent } from './user/template-form-controls/template-form-controls.component';
 
 const routs:Routes=[
   {path: 'home',component:HomeComponent,title:"Home- joes-robot-shop"},
   {path: 'catalog',component:CatalogComponent,title:"Catalog- joes-robot-shop"},
   {path: 'cart',component:CartComponent,title:"Cart- joes-robot-shop"},
   {path: 'sign-in',component:SignInComponent},
+  {path: 'form-controls',component:TemplateFormControlsComponent},
   {path: '', redirectTo:'/home', pathMatch: 'full'}
 ]
 
